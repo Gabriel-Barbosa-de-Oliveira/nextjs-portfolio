a6d7b722ca8d9496548d38136a1dd7a39adfcd18
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2023-01-20 20:20:45 -0300

    Dynamic Social Media List

--- next-portfolio/components/sidebar-social-media/SidebarSocialMedia.tsx
+++ next-portfolio/components/sidebar-social-media/SidebarSocialMedia.tsx
@@ -1,68 +1,31 @@
 import React, { useState } from 'react'
 import Image from 'next/image';
+import { socialMediaList } from '@/pages/api/socialMedia';
+import { ISocialMedia } from '@/interfaces/ISocialMedia';
 
 export default function SidebarSocialMedia() {
 
-  const [width, setWidth] = useState<number>(100);
-  const [height, setHeight] = useState<number>(30);
+  const socialList: Array<JSX.Element> = [];
+  socialMediaList.forEach((item: ISocialMedia) => {
+    socialList.push(
+      <li key={item.id}>
+        <a href={item.url} aria-label={item.ariaLabel} target="_blank" rel="noreferrer">
+          <Image
+            src={item.filePath}
+            alt={item.alt}
+            width={item.width}
+            height={item.height}
+            priority
+          />
+        </a>
+      </li>
+    )
+  });
 
   return (
     <nav className='fixed-sidebar'>
       <ul className='sidebar-container'>
-        <li>
-          <a href="https://github.com/Gabriel-Barbosa-de-Oliveira" aria-label="GitHub" target="_blank" rel="noreferrer">
-            <Image
-              src="/github.svg"
-              alt="Github Logo"
-              width={width}
-              height={height}
-              priority
-            />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.instagram.com/gab_barbosa_/" aria-label="Instagram" target="_blank" rel="noreferrer">
-            <Image
-              src="/instagram.svg"
-              alt="Instagram Logo"
-              width={width}
-              height={height}
-              priority
-            />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/gabriel-barbosa-de-oliveira/" aria-label="Linkedin" target="_blank" rel="noreferrer">
-            <Image
-              src="/linkedin.svg"
-              alt="LinkedIn Logo"
-              width={width}
-              height={height}
-              priority
-            />
-          </a>
-        </li>
-        <li>
-          <a href="https://twitter.com/gab_barbosa_dev" aria-label="Twitter" target="_blank" rel="noreferrer">
-            <Image
-              src="/twitter.svg"
-              alt="Twitter Logo"
-              width={width}
-              height={height}
-              priority
-            /></a>
-        </li>
-        <li>
-          <a href="https://medium.com/@gabriel-barbosa-de-oliveira" aria-label="Medium" target="_blank" rel="noreferrer">
-            <Image
-              src="/medium.svg"
-              alt="Medium Logo"
-              width={width}
-              height={height}
-              priority
-            />
-          </a>
-        </li>
+        {socialList}
       </ul>
     </nav>
   )
