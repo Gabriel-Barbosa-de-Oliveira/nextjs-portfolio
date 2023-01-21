07d3ce880a82f2365efc8e46d4f7c56f0023ddb7
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2023-01-20 21:14:50 -0300

    Nav Squeleton

--- next-portfolio/components/header/Header.tsx
+++ next-portfolio/components/header/Header.tsx
@@ -1,12 +1,28 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function Header() {
-  return (
-    <header className='header-container'>
-        <span>Gabriel Logo</span>
-        <nav>
-            Links
-        </nav>
-    </header>
-  )
+    return (
+        <header className='header-container'>
+            <div>
+                <a href='/'>
+                    <Image
+                        src="/gabriel.svg"
+                        alt="Gabriel Logo"
+                        width={42}
+                        height={42}
+                        priority
+                    />
+                </a>
+            </div>
+            <nav>
+                <ul> 
+                    <li>Sobre</li>
+                    <li>Experiencia</li>
+                    <li>Trabalho</li>
+                    <li>Contato</li>
+                </ul>
+            </nav>
+        </header>
+    )
 }
