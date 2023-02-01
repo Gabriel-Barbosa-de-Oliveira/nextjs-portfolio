e3849014f3d85538c2f136b2f9c4590176682b02
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2023-02-01 20:42:05 -0300

    Adjusting About Position

--- next-portfolio/components/about/About.tsx
+++ next-portfolio/components/about/About.tsx
@@ -1,9 +1,25 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function About() {
-  return (
-    <div className='entry-container'>
-        about
-    </div>
-  )
+    return (
+        <section className='about-container'>
+            <h1>Sobre</h1>
+            <div className='about-content'>
+                <div className='about-text'>
+                    <p>Oi tudo bem ? Meu nome é Gabriel Barbosa e sou apaixonado por criar aplicações para web</p>
+
+                </div>
+                <div className='about-image'>
+                    <Image
+                        src="/me.jpeg"
+                        alt="Gabriel Picture"
+                        width={200}
+                        height={250}
+                        priority
+                    />
+                </div>
+            </div>
+        </section>
+    )
 }
