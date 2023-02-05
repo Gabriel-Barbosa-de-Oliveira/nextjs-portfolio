fbb4a6f4f1d498fdf8909e4c1316352f0b078fa4
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2023-02-05 11:49:59 -0300

    Fix Url + priority

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  images: {
-    loader: "akamai",
-    path: "/",
-  }
-
 }
 
 module.exports = nextConfig
