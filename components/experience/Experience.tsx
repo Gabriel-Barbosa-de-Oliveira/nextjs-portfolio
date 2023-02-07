a07965c72d4307c6673a54593ad4a7dc7adc728d
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2023-02-06 21:02:04 -0300

    Dynamic Load Source Files

--- components/experience/Experience.tsx
+++ components/experience/Experience.tsx
@@ -1,47 +1,66 @@
+import { IExperience } from '@/interfaces/IExperience'
 import Image from 'next/image'
 import React, { Component } from 'react'
 
 export default class Experience extends Component {
+
+
   render() {
+    const experiences: Array<IExperience> = [
+      {
+        image: {
+          filePath: "/ibm.png",
+          alt: "IBM Logo",
+          height: 250,
+          width: 250
+        },
+        title: "IBM"
+      },
+      {
+        image: {
+          filePath: "/gsw.png",
+          alt: "GSW Logo",
+          height: 250,
+          width: 250
+        },
+        title: "GSW"
+      },
+      {
+        image: {
+          filePath: "/hipr.jpeg",
+          alt: "Hipr Sistemas",
+          height: 250,
+          width: 250
+        },
+        title: "Hipr"
+      }
+    ]
+
+    const enterprieseExperiences: Array<JSX.Element> = []
+
+    experiences.forEach((imageData: IExperience) => {
+      const { image } = imageData;
+      enterprieseExperiences.push(
+        <div className='image-container'>
+          <Image
+            key={imageData.title}
+            src={image.filePath}
+            alt={image.alt}
+            width={image.width}
+            height={image.height}
+          />
+          <span>{imageData.title}</span>
+        </div>
+      )
+    })
+
+
     return (
       <section className='experience-container'>
         <h1>Experiência</h1>
         <div className='experience-content'>
-          <div className='image-container'>
-            <Image
-              src="/ibm.png"
-              alt="IBM Logo"
-              width={250}
-              height={250}
-              priority
-            />
-
-            <span>IBM</span>
-          </div>
-          <div className='image-container'>
-            <Image
-              src="/gsw.png"
-              alt="Gsw Logo"
-              width={250}
-              height={250}
-              priority
-            />
-            <span>GSW</span>
-
-          </div>
-          <div className='image-container'>
-            <Image
-              src="/hipr.jpeg"
-              alt="Hipr Sistemas Logo"
-              width={250}
-              height={250}
-              priority
-            />
-            <span>Hipr Sistemas</span>
-          </div>
+          {enterprieseExperiences}
         </div>
-
-
       </section>
     )
   }
